d54fd84737614637534bd7d9073f79d2a8c08c58
Tzu Chao <chao.tzu5@gmail.com>
2017-12-20 10:04:04 -0500

    comment code

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -3,10 +3,13 @@ package quiz;
 public class ReverseString {
     public String reverse(String input) {
         ///return new StringBuffer(input).reverse().toString();
+    	// initialize a char array of the same length;
         char [] cha= new char[input.length()];
+        // enter chars from string in reverse order into char array
         for(int i=0;i<input.length();i++){
             cha[i]=input.charAt(input.length()-1-i);
         }
+        //return new string instance with char array as input
         return new String(cha);
     }
 }

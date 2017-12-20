91911a7d9d9fc3647544d35e6166a43787c99dda
Tzu Chao <chao.tzu5@gmail.com>
2017-12-20 10:01:08 -0500

    reverse done

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,11 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        ///return new StringBuffer(input).reverse().toString();
+        char [] cha= new char[input.length()];
+        for(int i=0;i<input.length();i++){
+            cha[i]=input.charAt(input.length()-1-i);
+        }
+        return new String(cha);
     }
 }
